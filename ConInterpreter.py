18dbef98d5be1c344674108361edab1e0746082e
Jhoan Gutiérrez <82736598+Blaskevorsz@users.noreply.github.com>
2023-09-26 11:17:59 -0500

    Update ConInterpreter.py

--- ConInterpreter.py
+++ ConInterpreter.py
@@ -1,29 +1,28 @@
-# Define la interfaz de la expresión
+
 class ExpresionBinaria:
     def interpretar(self):
         pass
 
-# Implementa las expresiones terminales
+
 class NumeroDecimal(ExpresionBinaria):
     def __init__(self, numero):
         # Llama al constructor de la clase base
         self.numero = numero
 
     def interpretar(self):
-        return bin(self.numero)[2:]  # Convierte el número decimal a binario
+        return bin(self.numero)[2:]  
 
-# Crea un contexto (opcional)
+
 class ContextoConversion:
     def __init__(self):
         self.resultado = ""
 
-# Uso del intérprete
 contexto = ContextoConversion()
 
 expresion = NumeroDecimal(10)
 contexto.resultado = expresion.interpretar()
-print("Representación binaria de 10:", contexto.resultado)  # Esto imprimirá "Representación binaria de 10: 1010"
+print("Representación binaria de 10:", contexto.resultado) 
 
 expresion = NumeroDecimal(23)
 contexto.resultado = expresion.interpretar()
-print("Representación binaria de 23:", contexto.resultado)  # Esto imprimirá "Representación binaria de 23: 10111"
+print("Representación binaria de 23:", contexto.resultado)  
