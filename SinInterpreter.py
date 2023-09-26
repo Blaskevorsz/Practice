5a61f0705d648d21f1a0f5858dc976d1b0e46f90
Jhoan Gutiérrez <82736598+Blaskevorsz@users.noreply.github.com>
2023-09-26 11:19:10 -0500

    Update SinInterpreter.py

--- SinInterpreter.py
+++ SinInterpreter.py
@@ -1,17 +1,13 @@
-# Define la clase NumeroDecimal sin usar el patrón Interpreter
+
 class NumeroDecimal:
     def __init__(self, numero):
         self.numero = numero
     
     def interpretar(self):
-        # Convertir el número decimal a binario y devolverlo
         return bin(self.numero)[2:]
 
-# Crear una instancia de NumeroDecimal
 numero_decimal = NumeroDecimal(10)
 
-# Llamar al método interpretar para obtener la representación binaria
 representacion_binaria = numero_decimal.interpretar()
 
-# Imprimir la representación binaria
-print(representacion_binaria)  # Esto imprimirá "1010"
+print(representacion_binaria) 
